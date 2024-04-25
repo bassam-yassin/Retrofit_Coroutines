38d5a81e0a289e59d43efc9c58f84bb93ff5fa39
bassam <bassam.alaa.yassin@gmail.com>
2024-04-25 07:16:45 +0200

    edit base url

--- app/src/main/java/com/example/retrofitcoroutinessession/util/Constants.kt
+++ app/src/main/java/com/example/retrofitcoroutinessession/util/Constants.kt
@@ -2,5 +2,5 @@ package com.example.retrofitcoroutinessession.util
 
 object Constants {
     const val API_KEY = "93bdd49224570f6e333e4c69fa6f5397"
-    const val BASE_URL = "https://api.themoviedb.org/3"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
 }
