a1ee0bcfb94682313e32fbe9cc3ce296794eeedb
bassam <bassam.alaa.yassin@gmail.com>
2024-04-25 23:21:10 +0200

    handle main activity

--- app/src/main/java/com/example/retrofitcoroutinessession/MainActivity.kt
+++ app/src/main/java/com/example/retrofitcoroutinessession/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
 import com.example.retrofitcoroutinessession.ui.movies.MoviesScreen
 import com.example.retrofitcoroutinessession.ui.movies.MoviesViewModel
@@ -22,6 +19,17 @@ class MainActivity : ComponentActivity() {
         val viewModel = ViewModelProvider(this).get(MoviesViewModel::class.java)
         setContent {
             RetrofitCoroutinesSessionTheme {
+
+//                val isSystemInDarkMode = isSystemInDarkTheme()
+//                val systemController = rememberSystemUiController()
+//
+//                SideEffect {
+//                    systemController.setSystemBarsColor(
+//                        color = Color.Transparent,
+//                        darkIcons = !isSystemInDarkMode,
+//                    )
+//                }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
