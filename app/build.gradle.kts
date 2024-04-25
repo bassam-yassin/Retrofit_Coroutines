3320b9305e3f397416dba61fd764f5b9ff1c8a5d
bassam <bassam.alaa.yassin@gmail.com>
2024-04-25 23:18:49 +0200

    handle gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,15 +70,10 @@ dependencies {
     //Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    // Coil
-    implementation("io.coil-kt:coil-compose:2.5.0")
-
-    //Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
 
 }
